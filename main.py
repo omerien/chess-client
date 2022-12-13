f40ed4a719612a2fdc5398c31c9a223f92a21259
omerien <53191414+omerien@users.noreply.github.com>
2022-12-13 09:01:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,21 @@ while _goodlogin == False:
       _goodlogin = True
   else:
     print("Server error. (code" + sessionid.status_code + ")")
+while _playing:
+  print("1. Join a match")
+  print("2. Play with a friend")
+  print("3. View stats")
+  print("4. Quit the game")
+  choice = input("What do you want to do ? ")
+  if choice != "1" and choice != "2" and choice != "3" and choice != "4":
+    # Do nothing lol
+    choice = "bruh"
+  elif choice == "1":
+    # Join a match
+  elif choice == "2":
+    friend = input("What is your friend username ? ")
+  elif choice == "3":
+    # Fetch stats
+  elif choice == "4":
+    _playing = False
+print("Bye!")
